ac2849de2e5b439eacdc4d69ac9dd43cd997c007
Skyler <skyedoesthings1@gmail.com>
2025-02-04 02:02:35 -0500

    formatting

--- static/js/vino.js
+++ static/js/vino.js
@@ -1,10 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.querySelector(".exit-button").addEventListener("click", function(){
-        if (window.vino) {
+/* eslint-disable no-undef */
+document.addEventListener("DOMContentLoaded", function () {
+    document
+        .querySelector(".exit-button")
+        .addEventListener("click", function () {
+            if (window.vino) {
                 vino.soundPlay("SE_COMMON_FINISH_TOUCH_OFF");
                 vino.exit();
-        } else {
-            alert("Exit app");
-        }
-    });
+            } else {
+                alert("Exit app");
+            }
+        });
 });
