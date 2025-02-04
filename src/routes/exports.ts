ee45645b935cd9ad5733626100e3524152b946d6
Skyler <skyedoesthings1@gmail.com>
2025-02-04 02:13:04 -0500

    Fix page, remove debug routes

--- src/routes/exports.ts
+++ src/routes/exports.ts
@@ -1,5 +1,4 @@
 import { vino } from "@/routes/ui/vino-jp";
-import { vinoDebug } from "@/routes/ui/debug";
 import { type Router } from "express";
 
 interface Routes {
@@ -13,11 +12,6 @@ const routes: Routes[] = [
         name: "Vino UI (JP)",
         path: "/",
         route: vino,
-    },
-    {
-        name: "Debug Vino UI (JP)",
-        path: "/debug",
-        route: vinoDebug,
     },
 ];
 
